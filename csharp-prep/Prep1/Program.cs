4e8903b4abadeef4d93a2757f43c212ec8fec10e
yrrab911 <barry.leidy@gmail.com>
2024-09-18 14:15:15 -0600

    completed the prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.Write($"Your name is {lname}, {fname} {lname}.");
     }
 }
