96af36071d0e1985c6e1a1a97b6164df80b87b7a
yrrab911 <barry.leidy@gmail.com>
2024-09-18 14:50:23 -0600

    completed prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,89 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your final grade? ");
+        string grades = Console.ReadLine();
+
+        string letter = ("");
+
+        int grade = int.Parse(grades);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+
+            if (grade < 93)
+            {
+                letter = "A-";
+            }
+
+            else
+
+            Console.WriteLine($"You got a {letter}!");
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+
+            if (grade < 83)
+            {
+                letter = "B-";
+            }
+
+            else if (grade >= 87)
+            {
+                letter = "B+";
+            }
+
+            else
+
+            Console.WriteLine($"You got a {letter}!");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+
+            if (grade < 73)
+            {
+                letter = "C-";
+            }
+
+            else if (grade >= 77)
+            {
+                letter = "C+";
+            }
+
+            Console.WriteLine($"You got a {letter}!");
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+
+            if (grade < 63)
+            {
+                letter = "D-";
+            }
+
+            else if (grade >= 67)
+            {
+                letter = "D+";
+            }
+
+            Console.WriteLine($"You got a {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}");
+        }
+
+        if (grade >= 70)
+        {
+            Console.Write("congratulations, you have passed this class!");
+        }
+
+        else
+        {
+            Console.Write("You have not passed this class, I'm sure you will pass next semester!");
+        }
     }
 }
