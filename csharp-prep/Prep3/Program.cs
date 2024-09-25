c9a4e0fc82bd4d924999784e8ee2dd4474cec067
yrrab911 <barry.leidy@gmail.com>
2024-09-25 16:03:53 -0600

    completed prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        while (play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magic = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+            int guesses = 0;
+
+            while (magic != guess)
+            {
+                guesses++;
+
+                
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                
+                if (magic <= guess)
+                {
+                    Console.WriteLine("lower");
+                }
+
+                else if (magic >= guess)
+                {
+                    Console.WriteLine("higher");
+                }
+
+                else
+                {
+                    Console.WriteLine("you got it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {guesses} guesses!");
+
+            Console.WriteLine("Would you like to play again");
+            play = Console.ReadLine();
+        }
+
     }
 }
