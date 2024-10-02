23fd8525fb51880b335a4a0b4bff4de380db2d7d
yrrab911 <barry.leidy@gmail.com>
2024-10-02 16:41:32 -0600

    completed learning 2

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Resume of: {_name}");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
