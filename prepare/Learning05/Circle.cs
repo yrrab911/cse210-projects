83f74471e7b17a31ce43eceb95234b8577187a28
yrrab911 <barry.leidy@gmail.com>
2024-11-13 14:06:26 -0700

    completed learning05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,13 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
